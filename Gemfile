f2cc9d4c8921e23e1e44d24f8874fef886158ac2
Brent <brent.ertz@gmail.com>
2011-06-30 20:19:20 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc1'
+gem 'heroku'
+gem 'pg'
+
+group :development do
+  gem 'ruby-debug19', :require => 'ruby-debug'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imprintz::Application.config.secret_token = 'a2040e3e731972002b91cc8ac06ba743c813af1eb4a319b91ad0bbc31e7b2c73c4e5feb5e33f51600124b69092a797cc0c41da9d8ff2e9c16fb6b842d1af4204'
