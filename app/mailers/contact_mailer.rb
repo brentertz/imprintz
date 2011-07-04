4b7c5c677de29dcda1e088da4660fc781231c692
Brent <brent.ertz@gmail.com>
2011-07-04 17:45:17 -0600

    Change site_email to contact_email in ContactsMailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,10 @@
 class ContactMailer < ActionMailer::Base
+  default from: CONFIG['contact_email']
+
   def contact_email(contact)
     @contact = contact
     mail(:from => contact.email,
-         :to => CONFIG['site_email'],
+         :to => CONFIG['contact_email'],
          :subject => "#{CONFIG['site_name']} contact!")
   end
 end
